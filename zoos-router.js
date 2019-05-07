ab78cb035208e7a32852ee3b4bc566951ef1d681
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-07 17:39:28 -0400

    finished post router and get by id

--- zoos-router.js
+++ zoos-router.js
@@ -12,12 +12,29 @@ const knexConfig = {
 const db = knex(knexConfig);
 //==================================================Create Router
 router.post("/", (req, res) => {
- 
+  if (!req.body.name) {
+    res.status(400).json({ message: `No Zoo name provided` });
+  } else {
+    db("zoos")
+      .insert(req.body)
+      .then(zoo => {
+          const [id] = zoo
+        db("zoos")
+          .where({ id })
+          .first()
+          .then(zoo => {
+            res.status(201).json(zoo);
+          })
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
 });
 
 //==================================================Read Router
 router.get("/", (req, res) => {
-     db(`zoos`)
+  db(`zoos`)
     .then(zoos => {
       res.status(200).json(zoos);
     })
@@ -27,7 +44,21 @@ router.get("/", (req, res) => {
 });
 
 //----------------------------------Read By ID
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: "zoo not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 //==================================================Update Router
 router.put("/:id", (req, res) => {});
